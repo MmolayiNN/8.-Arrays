72496efa41c0d807aede27afc89c5406d3ff4307
Hohm <Hohm@HOHM>
2024-01-22 20:30:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an array
+            int[] luckyNumbers = { 4, 5, 8, 9 };
+            Console.WriteLine(luckyNumbers[0]);
+            String[] friends = new string[5];
+            friends[0] = "Kim";
+            friends[1] = "Jabu";
+            friends[2] = "Nelca";
+            Console.WriteLine(friends[0]);
+
+
+        }
+    }
+}
